a9ff18f45815403f05da51724173a05535328370
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-08-16 22:55:50 +0530

    adding users servers

--- index.js
+++ index.js
@@ -15,11 +15,22 @@ app.get("/", (req, res)=>  {
     res.render("index", { name: "Himanshu"});
 });
 
+app.get("/success", (req, res) => {
+    res.render("success");
+});
+
+
 app.post("/", (req, res) => {
     user.push({ username: req.body.name, email: req.body.email});
 
-    res.redirect("/sucess");
-})
+    res.redirect("/success");
+});
+
+app.get("/users", (req, res) => {
+    res.json({
+        users,
+    });
+});
 
 app.listen(5000, () => {
     console.log("server is working");
