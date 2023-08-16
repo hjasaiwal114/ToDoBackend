5522baaa5dbe3e50412b37ed198d56cf551e6fda
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-08-16 17:39:48 +0530

    installing ejs file

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import express  from "express";
+import path from "path";
 
 const app = express();
+
+app.get("/", (req, res)=>{
+    const pathlocation = path.resolve();
+
+    res.render()
+});
 
 app.listen(5000, () => {
     console.log("server is working");
