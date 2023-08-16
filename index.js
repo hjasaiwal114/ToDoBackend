3314fbb28ad0e13838f9d829c77f9345a195661a
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-08-16 17:55:49 +0530

    creating files

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import express  from "express";
-import path from "path";
 
 const app = express();
+// setting up View engine
+app.set("view engine", "ejs");
 
-app.get("/", (req, res)=>{
-    const pathlocation = path.resolve();
-
-    res.render()
+app.get("/", (req, res)=>  {
+    res.render("index", {name: "Himanshu"});
 });
 
 app.listen(5000, () => {
