5f67f391762fa5ff64807ab1be02b0216de6749d
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-08-16 22:16:30 +0530

    creating file

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 import express  from "express";
-import path from 'path'
+import path from 'path';
 
 const app = express();
+// Using middle ware
+const user = [];
 
 app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({ extended: true}));
 
 // setting up View engine
 app.set("view engine", "ejs");
 
 app.get("/", (req, res)=>  {
-    res.render("index", {name: "Himanshu"});
+    res.render("index", { name: "Himanshu"});
 });
+
+app.post("/", (req, res) => {
+    user.push({ username: req.body.name, email: req.body.email});
+
+    res.redirect("/sucess");
+})
 
 app.listen(5000, () => {
     console.log("server is working");
