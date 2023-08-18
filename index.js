5c4d8ec0c16405c65558d1aa28536422224ae8f1
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-08-18 13:50:29 +0530

    adding to data base

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 import express  from "express";
+import mongoose from "mongoose";
 import path from 'path';
 
-const app = express();
-// Using middle ware
-const user = [];
 
+mongoose
+    .connect("mongodb://127.0.0.1:27017", {
+        dbName: "backend",
+})
+    .then(() => console.log("Database Connected"))
+    .catch((e) => console.log(e));
+
+const messageSchema = new mongoose.Schema({
+    name : String,
+    email: String,
+});
+
+const Message = mongoose.model("Message", messageSchema)
+
+const app = express();
+
+
+// Using middle ware
 app.use(express.static(path.join(path.resolve(), "public")));
 app.use(express.urlencoded({ extended: true}));
 
@@ -15,19 +31,19 @@ app.get("/", (req, res)=>  {
     res.render("index", { name: "Himanshu"});
 });
 
-app.get("/add", (req, res)=>  {
-    res.render("index", { name: "Himanshu"});
-});
+
 
 app.get("/success", (req, res) => {
     res.render("success");
 });
 
 
-app.post("/", (req, res) => {
-    user.push({ username: req.body.name, email: req.body.email});
+app.post("/contact", async(req, res) => {
 
-    res.redirect("/success");
+  const {name, email} = req.body;
+
+  await Message.create({ name, email});
+  res.redirect("/success");
 });
 
 app.get("/users", (req, res) => {
